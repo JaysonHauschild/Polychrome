1c66a129fb89071a5847ff858d1ed1a19f307ac9
Jayson Hauschild <jayson.a.hauschild@gmail.com>
2025-01-29 20:31:42 -0600

    Added an intro screen with the Polychrome Studios logo and a description of the app.

---
+++ Polychrome/NewsView.swift
@@ -0,0 +1,18 @@
+//
+//  NewsView.swift
+//  Polychrome
+//
+//  Created by Jayson Hauschild on 1/29/25.
+//
+
+import SwiftUI
+
+struct NewsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsView()
+}
