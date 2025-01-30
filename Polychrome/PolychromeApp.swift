c9bf984d6511d23217a3ca6a26d5329ada9c8e18
Jayson Hauschild <jayson.a.hauschild@gmail.com>
2025-01-29 19:03:14 -0600

    Initial Commit

---
+++ Polychrome/PolychromeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolychromeApp.swift
+//  Polychrome
+//
+//  Created by Jayson Hauschild on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct PolychromeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PolychromeTests/PolychromeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PolychromeTests.swift
+//  PolychromeTests
+//
+//  Created by Jayson Hauschild on 1/29/25.
+//
+
+import Testing
+@testable import Polychrome
+
+struct PolychromeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
